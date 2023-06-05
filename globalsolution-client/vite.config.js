86ffa7cd8037ed270e96e0f7016f5b675ab7c2b6
Lucas Blotta <lucas@blotta.info>
2023-06-05 03:29:04 -0300

    adding api url

--- globalsolution-client/vite.config.js
+++ globalsolution-client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: "https://localhost:7111"
-  }
+  // server: {
+  //   proxy: "https://localhost:7111"
+  // }
 })
